d5f0a3384637fe9a133e1794b544521f3bd1cb13
Kanisevsky <meltsmokestudio@gmail.com>
2024-01-30 19:06:43 +0000

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 const App = () => {
   return (
     <main className="bg-slate-300/20">
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={"home"}></Route>
-          <Route path="/about" element={"About"}></Route>
-          <Route path="/projects" element={"Projects"}></Route>
-          <Route path="/contact" element={"Contact"}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/projects" element={<Projects />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
         </Routes>
       </Router>
     </main>
