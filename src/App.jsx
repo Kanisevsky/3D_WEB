f137d6b82f56f168cd1e74f4b6c12a04244cdf41
Kanisevsky <meltsmokestudio@gmail.com>
2024-01-30 18:56:44 +0000

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-function App() {
-  return <div className="">APP</div>;
-}
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <main className="bg-slate-300/20">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={"home"}></Route>
+          <Route path="/about" element={"About"}></Route>
+          <Route path="/projects" element={"Projects"}></Route>
+          <Route path="/contact" element={"Contact"}></Route>
+        </Routes>
+      </Router>
+    </main>
+  );
+};
 
 export default App;
